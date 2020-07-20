2100a8740de1fd2fbe2260acc25010eef9d09eea
Elliot Parker <elliotccp@protonmail.com>
2020-07-20 14:01:03 +1000

    Update compiler

--- Bigger.java
+++ Bigger.java
@@ -1,22 +1,15 @@
-/**
- * @Elliot Parker
- * @v1.1.0-2020-07-11
- */
 import java.util.*;
 
-public class Bigger
-{
-    public static void main(String [] words) 
-    {
-        Scanner s = new Scanner(System.in);
+public class Bigger {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         System.out.print("Enter two numbers: ");
-        float f1 = s.nextFloat();
-        float f2 = s.nextFloat();
-        if(f2 >= f1*2)
-        {
+        float f1 = input.nextFloat();
+        float f2 = input.nextFloat();
+        input.close();
+        if (f2 >= f1 * 2) {
             System.out.println(f2 + " is two or more times bigger than " + f1);
-        } else
-        {
+        } else {
             System.out.println(f2 + " is not two or more times bigger than " + f1);
         }
     }
--- Twice.java
+++ Twice.java
@@ -1,17 +1,11 @@
-
-/**
- * @Elliot Parker
- * @v1.0.1-2020-07-11
- */
 import java.util.*;
 
-public class Twice
-{
-    public static void main(String [] words) 
-    {
-        Scanner s = new Scanner(System.in);
+public class Twice {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         System.out.print("Enter a number: ");
-        float f = s.nextFloat();
-        System.out.println(f + "*2=" + f*2);
+        float f = input.nextFloat();
+        input.close();
+        System.out.println(f + "*2=" + f * 2);
     }
 }
